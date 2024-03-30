24fdbbdd31bb857ba5e036453edee52e7ac0f619
Marek S <mareks@live.com>
2024-03-30 23:04:25 +0200

    first commit

---
+++ src/components/EndCallButton.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
+import React from "react";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+
+const EndCallButton = () => {
+  const call = useCall();
+  const router = useRouter();
+  const { useLocalParticipant } = useCallStateHooks();
+  const localParticiaipnt = useLocalParticipant();
+
+  const isMeetingOwner =
+    localParticiaipnt &&
+    call?.state.createdBy &&
+    localParticiaipnt.userId === call.state.createdBy.id;
+
+  if (!isMeetingOwner) return null;
+
+  return (
+    <Button
+      onClick={async () => {
+        await call.endCall();
+        router.push("/");
+      }}
+      className="bg-red-500"
+    >
+      End call for everyone
+    </Button>
+  );
+};
+
+export default EndCallButton;
